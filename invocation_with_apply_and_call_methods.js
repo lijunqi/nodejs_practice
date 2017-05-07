5c2b2d4b87b9a4a3263dc9cc8af699849fb254fe
Jacky <JackyLi@example.com>
2017-05-07 22:30:55 +0800

    bind function

--- invocation_with_apply_and_call_methods.js
+++ invocation_with_apply_and_call_methods.js
@@ -43,23 +43,93 @@ forEach(weapons, function(index){
 
 /*
  * Arrow functions pick up the value of the this parameter at the moment of their creation. 
- * arrow functions don't get their own implicit this parameter when we call them;
+ * 
+ * Arrow functions don't get their own implicit this parameter when we call them;
  * instead they remember the value of the this parameter at the time they were created.
+ * 
+ * But standard function not.
  */
+console.log('========= Arrow function callback =========');
+var elem = {
+    handler: function (btnClick) {
+        btnClick();
+    }
+};
+
 function Button() {
-    this.name = 'button';
+    this.clicked = false;
     this.click = () => {
-        console.log('button name is ' + this.name);
+        this.clicked = true;
     }
 }
-
 var btn1 = new Button();
-btn1.click();
+elem.handler(btn1.click);
+if (btn1.clicked === true) {
+    console.log('Arrow function Context is Button this.');
+}
 
-var btn2 = {
-    name: 'btn2',
+var btnObj1 = {
+    clicked: false,
     click: () => {
-        console.log('btn name is ' + this.name);
+        if (this === undefined) {
+            console.log('Undefined: Object literal Arrow function this === undefined.');
+        }
+        else {
+            this.clicked = true;
+        }
     }
 }
-btn2.click();
+elem.handler(btnObj1.click);
+if (btnObj1.clicked === true) {
+    console.log('Arrow function Context is Object literal this.');
+}
+else {
+    console.log('Arrow function Context is NOT Object literal this.');
+}
+
+
+// standard function
+console.log('========= Standard function callback =========');
+
+function Button2() {
+    this.clicked = false;
+    this.click = function () {
+        if (this === undefined) {
+            console.log('Undefined: Button2 standard function this === undefined.');
+        }
+        else {
+            this.clicked = true;
+        }
+    }
+}
+var btn2 = new Button2();
+elem.handler(btn2.click);
+if (btn2.clicked === true) {
+    console.log('Standard function use Button2 this.');
+}
+
+
+var btnObj2 = {
+    clicked: false,
+    click: function () {
+        if (this === undefined) {
+            console.log('Undefined: Object literal standard function this === undefined.');
+        }
+        else {
+            this.clicked = true;
+        }
+    }
+}
+elem.handler(btnObj2.click);
+if (btnObj2.clicked === true) {
+    console.log('Standard function Context is Object literal this.');
+}
+
+/* 
+ * Bind function creates a new function. This function has the same body,
+ * but its context is always bound to a certain object, regardless of the way we invoke it.
+ */
+elem.handler(btnObj2.click.bind(btnObj2));
+if (btnObj2.clicked === true) {
+    console.log('Bind Standard function Context is Object literal this.');
+}
