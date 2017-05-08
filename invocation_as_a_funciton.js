e0ac61042744f1f58a59d0ca9f493a1b910eabf7
lijunqi <lijunqi001@163.com>
2017-05-08 20:37:18 +0800

    test

--- invocation_as_a_funciton.js
+++ invocation_as_a_funciton.js
@@ -1,4 +1,18 @@
 
+/*
+ * - If a function is invoked as a function, the value of the this parameter is usually
+ *   the global window object in nonstrict mode, and undefined in strict mode.
+ * 
+ * – If a function is invoked as a method,
+ *   the value of the this parameter is usually the object on which the function was invoked.
+ * 
+ * – If a function is invoked as a constructor, the value of the this parameter is
+ *   the newly constructed object.
+ * 
+ * – If a function is invoked through call and apply, the value of the this
+ *   parameter is the first argument supplied to call and apply.
+*/
+
 function ninja() {
     return this; // __proto__ is null
 }
