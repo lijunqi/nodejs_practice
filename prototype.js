c7c7939199fc0ae993302aa8245ff2e721e573b0
lijunqi <lijunqi001@163.com>
2017-05-29 17:12:44 +0800

    add "prototype" and "closure"

---
+++ prototype.js
@@ -0,0 +1,59 @@
+'use strict'
+
+/* 
+ * 1. a function, when created, gets a new object
+ *    that's assigned to its prototype property
+ * 
+ * 2. When we use a function as a constructor (by calling new Ninja())
+ *    the prototype of the newly constructed object is set to
+ *    the object referenced by the constructor function's prototype
+ */
+
+/* * * prototype chain * * */
+{
+    const yoshi = { skulk: true };
+    const hattori = { sneak: true };
+    const kuma = { creep: true };
+
+    if ("creep" in yoshi) {
+        console.log('Yoshi can also creep.');
+    }
+    else {
+        console.log('Yoshi can not creep.');
+    }
+
+    // set hattori as prototype of yoshi
+    Object.setPrototypeOf(yoshi, hattori);
+    Object.setPrototypeOf(hattori, kuma);
+
+    if ("creep" in yoshi) {
+        console.log('Yoshi can also creep.');
+    }
+}
+
+
+/* * * Instance properties * * */
+{
+    // This isn't anything we'd advise doing in real-world code.
+    function Ninja() {
+        this.swung = false;
+        this.swingSword = function () {
+            console.log(`Instance swingSword. ${!this.swung}`);
+        };
+    }
+
+    Ninja.prototype.swingSword = function () {
+        console.log(`Prototype swingSword. ${this.swung}`);
+    }
+
+    const ninja = new Ninja();
+    ninja.swingSword();
+}
+
+{
+    function Ninja() {}
+    console.log(`type = ${typeof Ninja}`);
+    console.log(`type = ${typeof Ninja.prototype}`);
+    var a = [];
+    console.log(`type = ${typeof a}`);
+}
