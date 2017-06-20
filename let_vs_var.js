dbfbd963a6f218eeb3eaae524a70c57965f8f0b7
lijunqi <lijunqi001@163.com>
2017-06-20 22:17:00 +0800

    update let vs var

--- let_vs_var.js
+++ let_vs_var.js
@@ -1,5 +1,34 @@
 'use strict'
 
+/**
+ * Scoping rules
+ * Variables declared by let have as their scope the block in which
+ * they are defined, as well as in any contained sub-blocks.
+ * The main difference is that:
+ * the scope of a var variable is the entire enclosing function:
+ */
+
+function varTest() {
+  var x = 1;
+  if (true) {
+    var x = 2;  // same variable!
+    console.log('var x2 = ', x);  // 2
+  }
+  console.log('var x1 = ', x);  // 2
+}
+
+function letTest() {
+  let x = 1;
+  if (true) {
+    let x = 2;  // different variable
+    console.log('let x2 = ', x);  // 2
+  }
+  console.log('let x1 = ', x);  // 1
+}
+
+varTest();
+letTest();
+
 const Greeters = [];
 
 for (var i = 0; i < 10; i++)
