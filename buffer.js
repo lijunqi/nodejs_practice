3c14034b8f2dfe1b5f82c53fe431e46101f950d8
lijunqi <lijunqi001@163.com>
2017-07-24 21:57:33 +0800

    update buffer.js

--- buffer.js
+++ buffer.js
@@ -1,6 +1,8 @@
 'use strict'
 
-var buf = new Buffer('hello', 'binary');
+// Deprecated: new Buffer(string[, encoding])
+// Use Buffer.from(string[, encoding]) instead
+var buf = Buffer.from('hello', 'binary');
 
 console.log('buf = ', buf);
 
@@ -11,3 +13,9 @@ console.log('str = ', str);
 var binBuf = new Buffer(str, 'binary');
 
 console.log('binBuf = ', binBuf);
+
+// Deprecated: new Buffer(size)
+// Use Buffer.alloc(size[, fill[, encoding]]) instead
+// fill: avalue to pre-fill the new Buffer with. Default:0
+const zeroBuf = Buffer.alloc(5);
+console.log(zeroBuf)
